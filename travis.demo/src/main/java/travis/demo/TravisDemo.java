13e94586641621292dc75ca6b30c5f1775f958ae
arvindgupta1981 <arvind.mannindia@gmail.com>
2021-05-10 15:37:03 +0530

    Initial Demo Commit

---
+++ travis.demo/src/main/java/travis/demo/TravisDemo.java
@@ -0,0 +1,7 @@
+package travis.demo;
+
+public class TravisDemo {
+	public static void main(String...l) {
+		System.out.println("++++++++++++++++++++++++++++Travis Demo+++++++++++++++++++++++++++++++++++++++++");
+	}
+}
