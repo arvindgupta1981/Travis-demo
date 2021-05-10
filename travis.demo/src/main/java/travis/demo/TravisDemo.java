629a2326b0ad734077ba43daa38e761b4a311470
arvindgupta1981 <arvind.mannindia@gmail.com>
2021-05-10 15:42:37 +0530

    Traviss setup

--- travis.demo/src/main/java/travis/demo/TravisDemo.java
+++
@@ -1,7 +0,0 @@
-package travis.demo;
-
-public class TravisDemo {
-	public static void main(String...l) {
-		System.out.println("++++++++++++++++++++++++++++Travis Demo+++++++++++++++++++++++++++++++++++++++++");
-	}
-}
---
+++ traviss/src/main/java/traviss/Test.java
@@ -0,0 +1,7 @@
+package traviss;
+
+public class Test {
+	public static void main(String...l) {
+		System.out.println("++++++++++++++++++++++++++++++++Travis Demo.....");
+	}
+}
